cfd48cc07cc5f696a7f322e465af8a57e8d3b954
beca7777 <beca.victor@yahoo.com>
2024-06-10 12:44:49 +0300

    3 - am creat operatiile CRUD pt User - Mapper, Repository, Service si Exception

---
+++ src/main/java/com/users/exceptions/ExceptionResponse.java
@@ -0,0 +1,11 @@
+package com.users.exceptions;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class ExceptionResponse {
+    private LocalDateTime timeStamp;
+    private String message;
+    private int errorCode;
+}
